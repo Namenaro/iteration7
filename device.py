1210dd0ce44dfe08834243e1443a5f6a5b7c835c
neuro <nanenaro@gmail.com>
2021-05-03 15:48:59 +0300

    Initial commit

---
+++ device.py
@@ -0,0 +1,11 @@
+
+
+class Device:
+    def __init__(self):
+        pass
+
+    def get_level_of_activation(self, signal, point):
+        pass
+
+    def get_value_of_activation(self,  signal, point):
+        pass
